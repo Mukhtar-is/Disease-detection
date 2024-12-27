7910f85cd2b4b31e7af8e72f795950f22971b98c
Mukhtaar Ismail <68457126+Mukhtar-is@users.noreply.github.com>
2024-12-27 08:46:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 
 
-#Tensorflow Model Prediction
+#Tensorflow Model
 def model_prediction(test_image):
 #    model = tf.keras.models.load_model("trained_maize_disease_model.keras")
     model = tf.keras.models.load_model("maize_disease_detection_model.h5")
@@ -26,7 +26,7 @@ app_mode = st.sidebar.selectbox("Select Page",["Home","About Us","Disease Detect
 if(app_mode=="Home"):
     st.header("PLANT DISEASE RECOGNITION SYSTEM")
     image_path = "Home.JPG"
-    st.image(image_path,use_column_width=True)
+    st.image(image_path,use_container_width=True)
     st.markdown("""
     Welcome to the Plant Disease Recognition System! 
     
@@ -66,7 +66,7 @@ elif(app_mode=="Disease Detection"):
     st.header("Disease Detection")
     test_image = st.file_uploader("Choose an Image:")
     if(st.button("Show Image")):
-        st.image(test_image,width=4,use_column_width=True)
+        st.image(test_image,width=4,use_container_width=True)
     #Predict button
     if(st.button("Predict")):
         st.snow()
